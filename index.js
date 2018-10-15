ac8522ba5a340d818fe21c2ebb6cae1eb9da3f33
leandro-viana <leandrovianadc@gmail.com>
2018-10-15 09:34:05 -0300

    End of the Cap1
    At the end of the cap 1, we have this file.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// cliet/app/index.js
+// client/app/index.js
 
 var campos = [
     document.querySelector('#data'),
@@ -7,3 +7,39 @@ var campos = [
 ];
 
 console.log(campos); //verificando o conteúdo do array
+
+// precisamos de tbody, pois ele receberá a tr que vamos construir
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event){
+    //cancelando a submissão do formulário
+    event.preventDefault();   
+    var tr = document.createElement('tr');
+    campos.forEach(function(campo) {
+        //cria uma td sem informações
+        var td = document.createElement('td');
+        //atribui o valor do campo à td
+        td.textContent = campo.value;
+        //adiciona a td na tr
+        tr.appendChild(td);
+    });
+    //nova td que armazenará o volume da negociação
+    var tdVolume = document.createElement('td');
+    //as posições 1 e 2 do array armazenam os campos de quantidade e valor, respectivamente
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    //adicionando a td que faltava à tr
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+    //limpa o campo da data
+    campos[0].value = '';
+    //limpa quantidade
+    campos[1].value = 1;
+    //limpa valor
+    campos[2].value = 0;
+    //foca no campo da data
+    campos[0].focus();
+});
+
+
