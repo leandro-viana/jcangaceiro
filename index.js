b89dbaeae44fd20a8f2582654c165e22811ffb10
leandro-viana <leandrovianadc@gmail.com>
2018-10-15 08:31:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// cliet/app/index.js
+
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+console.log(campos); //verificando o conteúdo do array
