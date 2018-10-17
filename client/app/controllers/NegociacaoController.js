e0b738c17e6a9a395458aa1c18adfbe99f0f4fed
leandro-viana <leandrovianadc@gmail.com>
2018-10-17 09:36:20 -0300

    Cap6
    Ending of the Cap6, initiate the class NegociacoesView to create views.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,11 +6,16 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
+        // passamos para o construtor o seletor CSS  de ID
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        //atualizando a view, recebe inicialmente o modelo que encapsula uma lista vazia
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event){
         // cancelando a submissão do formulário
         event.preventDefault();
         this._negociacoes.adiciona(this._CriaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
         // limpando formulário
         this._limparFormulario();
         
