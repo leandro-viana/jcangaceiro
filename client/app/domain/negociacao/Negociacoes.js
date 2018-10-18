c15b1f88c2de5aff61e1f30bf34d0b0cfc683c1a
leandro-viana <leandrovianadc@gmail.com>
2018-10-18 10:51:11 -0300

    Cap9
    End of the Cap9

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,10 +1,10 @@
 // client/app/domain/negociacao/Negociacoes.js
 
 class Negociacoes{
-    constructor(armadilha){
+    constructor(){
         this._negociacoes = [];
         // estratégia de atualização para ser chamada toda vez que algum método alterar o estado interno
-        this._armadilha = armadilha;
+        //this._armadilha = armadilha;
         // Congela a instância para impedir novas atribuições às propriedades da instância de um objeto
         Object.freeze(this);
     }
@@ -12,7 +12,6 @@ class Negociacoes{
         // Não funciona se o object.freeze foi usado no construtor - >this._negociacoes.push(negociacao);
         // Nesse caso deve se utilizar -> this._negociacoes.length = 0;
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
     }
     paraArray(){
         // retorna uma nova referência criada com os itens de this._negociacoes
@@ -33,7 +32,6 @@ class Negociacoes{
     }
     // Atribui um novo array sem elementos(vazio) à propriedade this._negociacoes quando o usuário clicar em "Apagar"
     esvazia(){
-        this._negociacoes = [];
-        this._armadilha(this);
+        this._negociacoes.length = 0;
     }
 }
