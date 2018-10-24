e4a0a142b8c612630ee8e900303ee376c1e6a404
leandro-viana <leandrovianadc@gmail.com>
2018-10-24 11:27:30 -0300

    Cap17
    Cap17 start using Bareel to simplifying the import of modules.

---
+++ app-src/ui/models/Mensagem.js
@@ -0,0 +1,13 @@
+// client/ap/ui/models/Mensagem.js
+export class Mensagem{
+    // se não for passado, será uma string em branco
+    constructor(texto=''){
+        this._texto = texto;
+    }
+    get texto(){
+        return this._texto;
+    }
+    set texto(texto){
+        this._texto = texto;
+    }
+}
